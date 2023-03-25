8949846b6f0c979078ba7710cfe801c96f6fc28b
Duman Makulbekov <makulbekov.d@gmail.com>
2023-03-25 19:18:52 +0600

    010 Использование типов

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+let revenue = 1000;
+let bonus = 500;
+let a = 'test';
+let b = true;
+let res = revenue + bonus;
+console.log(res);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let revenue: number = 1000;
+let bonus: number = 500;
+let a: string = 'test';
+let b: boolean = true;
+
+let res = revenue + bonus;
+console.log(res);
