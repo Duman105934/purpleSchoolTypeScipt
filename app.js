32dd41b5d6b8842df456134ab693cb8c0b5f3272
Duman Makulbekov <makulbekov.d@gmail.com>
2023-03-26 16:45:34 +0600

    022 Type Aliases

--- app.js
+++ app.js
@@ -1,37 +1,25 @@
 "use strict";
-// Literalny type dannih
-function fethWithAuth(url, method) { }
-fethWithAuth('some url', 'get'); // method mozhno peedat tolko 'post' | 'get'
-// analog literalnyh typov
-// enum RequestType {
-// 	GET = 'get',
-// 	POST = 'post'
-// }
-// function fethWithAuth(url: string, method: RequestType) {
-// };
-// Vozvroshaemy Literalny type dannih
-function fethWithAuth_2(url, method) {
+// union type (cherez '|')
+// type - est tolko v TSC, v JS ego ne budet
+function fethWithAuth(url, method) {
     return 1; // mozhno vernut tolko 1 ili -1
 }
-fethWithAuth('some url', 'get'); // method mozhno peedat tolko 'post' | 'get'
-// Privedenie let k konst
-let a = 'some text';
-//a = 'eny text'
-// mozhno prisvoit tolko 'some text' tak kak eto znachenie prisvoen kak type
-// po etomu a = 'eny text' ne srabotaet
-//----------------------------------------------------------------------------
-function fethWithAuth_3(url, some_method) { }
 fethWithAuth('some url', 'post');
-let some_method = 'post';
-//fethWithAuth('some url', some_method); // comment chtoby sdelat tsc
-/*
-prisvoit method vmesto 'post' tak kak u nego vyshe prisvoeno 'post'
-nelzya tak kak u nego klass 'string' a ne 'post' (primer s a: 1 = 1)
-*/
-// obhodnoe reshenie: var 1
-//const some_method = 'post'; // let zamenit na const
-// obhodnoe reshenie: var 2
-//fethWithAuth('some url', some_method as 'post'); // costanut cherez 'as'
-/*
-s cast 'as' nado byt akuratnee tak kak vmesto 'post' mozhno peredat chto to drugoe togozhe type
-*/
+let user = {
+    name: 'Duman',
+    age: 35,
+    skils: ['Dev', 'noDev'],
+};
+let user_2 = {
+    name: 'Duman',
+    age: 35,
+    skils: ['Dev', 'noDev'],
+    id: 1,
+};
+// let user_3: USER_2WITHROLE_2 = {
+//   name: 'Duman',
+//   age: 35,
+//   skils: ['Dev', 'noDev'],
+//   name: 'Admin',
+//   id: 1,
+// };
--- app.ts
+++ app.ts
@@ -1,46 +1,76 @@
-// Literalny type dannih
-function fethWithAuth(url: string, method: 'post' | 'get') {}
-fethWithAuth('some url', 'get'); // method mozhno peedat tolko 'post' | 'get'
-// analog literalnyh typov
+// union type (cherez '|')
 
-// enum RequestType {
-// 	GET = 'get',
-// 	POST = 'post'
-// }
+type httpMethod = 'post' | 'get'; // mozhno otdelno obyavit type dannyh
+// type - est tolko v TSC, v JS ego ne budet
 
-// function fethWithAuth(url: string, method: RequestType) {
-// };
-
-// Vozvroshaemy Literalny type dannih
-function fethWithAuth_2(url: string, method: 'post' | 'get'): 1 | -1 {
+function fethWithAuth(url: string, method: httpMethod): 1 | -1 {
   return 1; // mozhno vernut tolko 1 ili -1
 }
-fethWithAuth('some url', 'get'); // method mozhno peedat tolko 'post' | 'get'
 
-// Privedenie let k konst
-let a: 'some text' = 'some text';
-//a = 'eny text'
-// mozhno prisvoit tolko 'some text' tak kak eto znachenie prisvoen kak type
-// po etomu a = 'eny text' ne srabotaet
-
-//----------------------------------------------------------------------------
-
-function fethWithAuth_3(url: string, some_method: 'post' | 'get') {}
 fethWithAuth('some url', 'post');
 
-let some_method = 'post';
-//fethWithAuth('some url', some_method); // comment chtoby sdelat tsc
-/*
-prisvoit method vmesto 'post' tak kak u nego vyshe prisvoeno 'post' 
-nelzya tak kak u nego klass 'string' a ne 'post' (primer s a: 1 = 1)
-*/
+// obyavlenie odnorodnogo type
+type coolString = string; // coolString mozhno vstavit kak string
 
-// obhodnoe reshenie: var 1
-//const some_method = 'post'; // let zamenit na const
+//--------------------------------------------------------------------------------------
 
-// obhodnoe reshenie: var 2
-//fethWithAuth('some url', some_method as 'post'); // costanut cherez 'as'
+// type dlya object
+type USER = {
+  name: string;
+  age: number;
+  skils: string[];
+};
 
-/*
-s cast 'as' nado byt akuratnee tak kak vmesto 'post' mozhno peredat chto to drugoe togozhe type
-*/
+let user: USER = {
+  name: 'Duman',
+  age: 35,
+  skils: ['Dev', 'noDev'],
+};
+
+// type dlya intersection (obedenenny iz neskolkih type objects)
+type USER_2 = {
+  name: string;
+  age: number;
+  skils: string[];
+};
+
+type ROLE = {
+  id: number;
+};
+
+type USER_2WITHROLE = USER_2 & ROLE; // esli znak '|' ili, esli znak '&' eto i (USER_2 i ROLE)
+
+let user_2: USER_2WITHROLE = {
+  name: 'Duman',
+  age: 35,
+  skils: ['Dev', 'noDev'],
+  id: 1,
+};
+
+// type dlya intersection s peresekaushimisya svoistvamy
+// (obedenenny iz neskolkih type objects)
+// esli est peresecheniya odin udalitsya
+
+type USER_3 = {
+  name: string; // imya polzovatelya, peresekaetsya s type ROLE_2
+  age: number;
+  skils: string[];
+};
+
+type ROLE_2 = {
+  name: string; // imya roly, peresekaetsya s type USER_3
+  id: number;
+};
+
+type USER_2WITHROLE_2 = {
+  user: USER_3;
+  role: ROLE_2;
+};
+
+// let user_3: USER_2WITHROLE_2 = {
+//   name: 'Duman',
+//   age: 35,
+//   skils: ['Dev', 'noDev'],
+//   name: 'Admin',
+//   id: 1,
+// };
