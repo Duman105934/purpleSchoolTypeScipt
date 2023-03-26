44b4411834abe8e6387b531760d723f95e63dc68
Duman Makulbekov <makulbekov.d@gmail.com>
2023-03-26 13:07:23 +0600

    017 Enums

--- app.js
+++ app.js
@@ -1,9 +1,65 @@
 "use strict";
-// var 1: koroti variant zapisi readonly
-const skill = [1, 'Dev']; // kombinaciya tipov
-// const skils: readonly string[] = ['Dev', 'DevOps']; // odinarny tip, esli odinarny tip pishetsya vne skobok
-//skils[0] = '' // nelzya izmenit esli readonly
-//skils.push('novoe znachenie') // nelzya dobavit esli readonly
-// var 2: dop variant
-const skils = ['Dev', 'DevOps']; // kak generic
-//const skils: Array<string> = ['Dev', 'DevOps']; // generic
+// var 1: chislovie enum
+// enum StatusCode {
+//   SUCCESS = 1,
+//   IN_PROCESS = 2,
+//   FAILED = 3,
+// }
+// const result = {
+//   message: 'Платеж успешен',
+//   statusCode: StatusCode.SUCCESS,
+// };
+// // 1 = успех
+// // 2 = в процессе
+// // 3 = отклонен
+// if (result.statusCode === StatusCode.SUCCESS) {
+//   console.log('Vse OK');
+// }
+// var 2: strokoviye enum
+// enum StatusCode {
+//   SUCCESS = 's',
+//   IN_PROCESS = 'p',
+//   FAILED = 'f',
+// }
+// const result = {
+//   message: 'Платеж успешен',
+//   statusCode: StatusCode.SUCCESS,
+// };
+// // 's' = успех
+// // 'p' = в процессе
+// // 'f' = отклонен
+// if (result.statusCode === StatusCode.SUCCESS) {
+//   console.log('Vse OK');
+// }
+// var 3: geterogennie enum
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode["IN_PROCESS"] = "p";
+    StatusCode["FAILED"] = "f";
+})(StatusCode || (StatusCode = {}));
+const result = {
+    message: 'Платеж успешен',
+    statusCode: StatusCode.SUCCESS,
+};
+// 1 = успех
+// 'p' = в процессе
+// 'f' = отклонен
+if (result.statusCode === StatusCode.SUCCESS) {
+    console.log('Vse OK');
+}
+function action(status) { }
+action(StatusCode.SUCCESS);
+action(1);
+//action('p');  // takoi var ne budet rabotat,
+// tak kak enum hot i generic no vse eshe ostaetsya number
+function compute() {
+    return 3;
+}
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 1] = "ADMIN";
+    Roles[Roles["MANAGER"] = 2] = "MANAGER";
+    Roles[Roles["USER"] = compute()] = "USER";
+})(Roles || (Roles = {}));
+const result_2 = 1 /* Roles_2.ADMIN */;
--- app.ts
+++ app.ts
@@ -1,11 +1,88 @@
-// var 1: koroti variant zapisi readonly
+// var 1: chislovie enum
 
-const skill: readonly [number, string] = [1, 'Dev']; // kombinaciya tipov
-// const skils: readonly string[] = ['Dev', 'DevOps']; // odinarny tip, esli odinarny tip pishetsya vne skobok
-//skils[0] = '' // nelzya izmenit esli readonly
-//skils.push('novoe znachenie') // nelzya dobavit esli readonly
+// enum StatusCode {
+//   SUCCESS = 1,
+//   IN_PROCESS = 2,
+//   FAILED = 3,
+// }
 
-// var 2: dop variant
+// const result = {
+//   message: 'Платеж успешен',
+//   statusCode: StatusCode.SUCCESS,
+// };
 
-const skils: ReadonlyArray<string> = ['Dev', 'DevOps']; // kak generic
-//const skils: Array<string> = ['Dev', 'DevOps']; // generic
+// // 1 = успех
+// // 2 = в процессе
+// // 3 = отклонен
+
+// if (result.statusCode === StatusCode.SUCCESS) {
+//   console.log('Vse OK');
+// }
+
+// var 2: strokoviye enum
+
+// enum StatusCode {
+//   SUCCESS = 's',
+//   IN_PROCESS = 'p',
+//   FAILED = 'f',
+// }
+
+// const result = {
+//   message: 'Платеж успешен',
+//   statusCode: StatusCode.SUCCESS,
+// };
+
+// // 's' = успех
+// // 'p' = в процессе
+// // 'f' = отклонен
+
+// if (result.statusCode === StatusCode.SUCCESS) {
+//   console.log('Vse OK');
+// }
+
+// var 3: geterogennie enum
+
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 'p',
+  FAILED = 'f',
+}
+
+const result = {
+  message: 'Платеж успешен',
+  statusCode: StatusCode.SUCCESS,
+};
+
+// 1 = успех
+// 'p' = в процессе
+// 'f' = отклонен
+
+if (result.statusCode === StatusCode.SUCCESS) {
+  console.log('Vse OK');
+}
+
+function action(status: StatusCode) {}
+
+action(StatusCode.SUCCESS);
+action(1);
+//action('p');  // takoi var ne budet rabotat,
+// tak kak enum hot i generic no vse eshe ostaetsya number
+
+function compute() {
+  return 3;
+}
+
+enum Roles {
+  ADMIN = 1,
+  MANAGER = 1 + 1, // mozhno delat rasxhety s chislamy
+  USER = compute(), // mozhno vizivat function
+}
+
+// constantny enum
+
+const enum Roles_2 {
+  ADMIN = 1,
+  MANAGER = 2,
+}
+
+const result_2 = Roles_2.ADMIN;
