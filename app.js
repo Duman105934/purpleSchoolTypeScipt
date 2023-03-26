a87e292e1a15d454c14c69de7cbc359c9fe106e9
Duman Makulbekov <makulbekov.d@gmail.com>
2023-03-26 13:33:57 +0600

    018 Упражнение - Типизируем функцию

--- app.js
+++ app.js
@@ -1,65 +1,41 @@
 "use strict";
-// var 1: chislovie enum
-// enum StatusCode {
-//   SUCCESS = 1,
-//   IN_PROCESS = 2,
-//   FAILED = 3,
-// }
-// const result = {
-//   message: 'Платеж успешен',
-//   statusCode: StatusCode.SUCCESS,
-// };
-// // 1 = успех
-// // 2 = в процессе
-// // 3 = отклонен
-// if (result.statusCode === StatusCode.SUCCESS) {
-//   console.log('Vse OK');
-// }
-// var 2: strokoviye enum
-// enum StatusCode {
-//   SUCCESS = 's',
-//   IN_PROCESS = 'p',
-//   FAILED = 'f',
-// }
-// const result = {
-//   message: 'Платеж успешен',
-//   statusCode: StatusCode.SUCCESS,
-// };
-// // 's' = успех
-// // 'p' = в процессе
-// // 'f' = отклонен
-// if (result.statusCode === StatusCode.SUCCESS) {
-//   console.log('Vse OK');
-// }
-// var 3: geterogennie enum
-var StatusCode;
-(function (StatusCode) {
-    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
-    StatusCode["IN_PROCESS"] = "p";
-    StatusCode["FAILED"] = "f";
-})(StatusCode || (StatusCode = {}));
-const result = {
-    message: 'Платеж успешен',
-    statusCode: StatusCode.SUCCESS,
+// moi variant
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-// 1 = успех
-// 'p' = в процессе
-// 'f' = отклонен
-if (result.statusCode === StatusCode.SUCCESS) {
-    console.log('Vse OK');
+// async function getFaqs(req): Promise<{
+// 	question: string;
+// 	answer: string,
+// 	tags: string[],
+// 	likes: number,
+// 	status: string > {
+//   const res = await fetch('/faqs', {
+//     method: 'POST',
+//     body: JSON.stringify(req),
+//   });
+//   const data = await res.json();
+//   return data;
+// }
+// pravilny variant
+var QuestionStatus;
+(function (QuestionStatus) {
+    QuestionStatus["PUBLISHED"] = "published";
+    QuestionStatus["DRAFT"] = "draft";
+    QuestionStatus["DELETED"] = "deleted";
+})(QuestionStatus || (QuestionStatus = {}));
+function getFaqs(req) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield fetch('/faqs', {
+            method: 'POST',
+            body: JSON.stringify(req),
+        });
+        const data = yield res.json();
+        return data;
+    });
 }
-function action(status) { }
-action(StatusCode.SUCCESS);
-action(1);
-//action('p');  // takoi var ne budet rabotat,
-// tak kak enum hot i generic no vse eshe ostaetsya number
-function compute() {
-    return 3;
-}
-var Roles;
-(function (Roles) {
-    Roles[Roles["ADMIN"] = 1] = "ADMIN";
-    Roles[Roles["MANAGER"] = 2] = "MANAGER";
-    Roles[Roles["USER"] = compute()] = "USER";
-})(Roles || (Roles = {}));
-const result_2 = 1 /* Roles_2.ADMIN */;
--- app.ts
+++ app.ts
@@ -1,88 +1,45 @@
-// var 1: chislovie enum
+// moi variant
 
-// enum StatusCode {
-//   SUCCESS = 1,
-//   IN_PROCESS = 2,
-//   FAILED = 3,
+// async function getFaqs(req): Promise<{
+// 	question: string;
+// 	answer: string,
+// 	tags: string[],
+// 	likes: number,
+// 	status: string > {
+//   const res = await fetch('/faqs', {
+//     method: 'POST',
+//     body: JSON.stringify(req),
+//   });
+//   const data = await res.json();
+//   return data;
 // }
 
-// const result = {
-//   message: 'Платеж успешен',
-//   statusCode: StatusCode.SUCCESS,
-// };
+// pravilny variant
 
-// // 1 = успех
-// // 2 = в процессе
-// // 3 = отклонен
-
-// if (result.statusCode === StatusCode.SUCCESS) {
-//   console.log('Vse OK');
-// }
-
-// var 2: strokoviye enum
-
-// enum StatusCode {
-//   SUCCESS = 's',
-//   IN_PROCESS = 'p',
-//   FAILED = 'f',
-// }
-
-// const result = {
-//   message: 'Платеж успешен',
-//   statusCode: StatusCode.SUCCESS,
-// };
-
-// // 's' = успех
-// // 'p' = в процессе
-// // 'f' = отклонен
-
-// if (result.statusCode === StatusCode.SUCCESS) {
-//   console.log('Vse OK');
-// }
-
-// var 3: geterogennie enum
-
-enum StatusCode {
-  SUCCESS = 1,
-  IN_PROCESS = 'p',
-  FAILED = 'f',
+enum QuestionStatus {
+  PUBLISHED = 'published',
+  DRAFT = 'draft',
+  DELETED = 'deleted',
 }
 
-const result = {
-  message: 'Платеж успешен',
-  statusCode: StatusCode.SUCCESS,
-};
-
-// 1 = успех
-// 'p' = в процессе
-// 'f' = отклонен
-
-if (result.statusCode === StatusCode.SUCCESS) {
-  console.log('Vse OK');
+async function getFaqs(req: {
+  // zapros
+  topicId: number;
+  status?: QuestionStatus; // optional - mozhet byt mozhet i ne byt
+}): Promise<
+  {
+    // otvet
+    question: string;
+    answer: string;
+    tags: string[];
+    likes: number;
+    status: QuestionStatus;
+  }[]
+> {
+  const res = await fetch('/faqs', {
+    method: 'POST',
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+  return data;
 }
-
-function action(status: StatusCode) {}
-
-action(StatusCode.SUCCESS);
-action(1);
-//action('p');  // takoi var ne budet rabotat,
-// tak kak enum hot i generic no vse eshe ostaetsya number
-
-function compute() {
-  return 3;
-}
-
-enum Roles {
-  ADMIN = 1,
-  MANAGER = 1 + 1, // mozhno delat rasxhety s chislamy
-  USER = compute(), // mozhno vizivat function
-}
-
-// constantny enum
-
-const enum Roles_2 {
-  ADMIN = 1,
-  MANAGER = 2,
-}
-
-const result_2 = Roles_2.ADMIN;
