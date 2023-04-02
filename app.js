6f8255a757d95d086041fc9d201ac743015431fe
Duman Makulbekov <makulbekov.d@gmail.com>
2023-04-02 10:44:09 +0600

    Revert "026 Упражнение - Типизируем ответ сервера"
    This reverts commit 15ecf3bd0750af9642c4d011b99e9210555ee898.

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 "use strict";
 const user = {
-    login: 'mail@mail.ru',
-    password: '', // нет данных, знак ? дает возможность не указывать обязательные
+    name: 'Duman',
+    age: 35,
 };
-// --------------------
-function multiply(first, second) {
-    if (!second) {
-        return first * first;
-    }
-    return first * second;
-}
-console.log(multiply(5));
-function testPas(user) {
-    var _a;
-    const t = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type; // = user.password ? user.password.type : undefined;
-}
-// --------------------
-function test(param) {
-    const t = param !== null && param !== void 0 ? param : multiply(5); // если param = null | undefined => multiply(5) короткий вариант записи
-}
+// использовать желательно только с простыми типами и union
--- app.ts
+++ app.ts
@@ -1,39 +1,19 @@
 interface User {
-  login: string;
-  password?: string;
+  name: string;
+}
+
+interface User {
+  age: number;
 }
 
 const user: User = {
-  login: 'mail@mail.ru',
-  password: '', // нет данных, знак ? дает возможность не указывать обязательные
+  name: 'Duman',
+  age: 35,
 };
 
-// --------------------
+// Польза от интерфейса можно доопредеять и дополнять свойства, но лучше так делать только
+// тогда когда библиотека сторонняя
 
-function multiply(first: number, second?: number): number {
-  if (!second) {
-    return first * first;
-  }
-  return first * second;
-}
+type ID = string | number;
 
-console.log(multiply(5));
-
-// --------------------
-
-interface UserPro {
-  login: string;
-  password?: {
-    type: 'primaty' | 'secondary';
-  };
-}
-
-function testPas(user: UserPro) {
-  const t = user.password?.type; // = user.password ? user.password.type : undefined;
-}
-
-// --------------------
-
-function test(param?: string) {
-  const t = param ?? multiply(5); // если param = null | undefined => multiply(5) короткий вариант записи
-}
+// использовать желательно только с простыми типами и union
