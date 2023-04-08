82f83537715f67ad3c176beaae2ab793bf855b52
Duman Makulbekov <makulbekov.d@gmail.com>
2023-04-08 12:07:54 +0600

    033 Упражнение - Делаем typeguard ответа

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
 "use strict";
-const user = {
-    name: 'Вася',
-    email: 'vasily@yandex.ru',
-    login: 'Vasia',
-};
-function logId(id) {
-    if (typeof id === 'string') {
-        console.log(id); // string
-    }
-    else {
-        console.log(id); // number
-    }
-    //id; // opyat string | number (flow typov)
-}
-// prostaya forma napisanya funccii type guard
-function isString(x) {
-    return typeof x === 'string';
-}
-// vmesto typeof id === 'string'
-function logId_2(id) {
-    if (isString(id)) {
-        console.log(id); // string
-    }
-    else {
-        console.log(id); // number
-    }
-}
-// bolee slozhny variant ispolzovanya type guard
-// sperva functiya type guard
 function isAdmin(user) {
     return 'role' in user;
 }
@@ -38,8 +9,4 @@ function setRoleZero(user) {
     else {
         throw new Error('пользователь не Админ');
     }
-}
-// vtoroi variant napisaniya isAdmin
-function isAdminAlternative(user) {
-    return user.role !== undefined; // cherez prinuditelnoe castovanie
 }
--- app.ts
+++ app.ts
@@ -4,43 +4,11 @@ interface User {
   login: string;
 }
 
-const user: User = {
-  name: 'Вася',
-  email: 'vasily@yandex.ru',
-  login: 'Vasia',
-};
-
 interface Admin {
   name: string;
   role: number;
 }
 
-function logId(id: string | number) {
-  if (typeof id === 'string') {
-    console.log(id); // string
-  } else {
-    console.log(id); // number
-  }
-  //id; // opyat string | number (flow typov)
-}
-
-// prostaya forma napisanya funccii type guard
-function isString(x: string | number): x is string {
-  return typeof x === 'string';
-}
-
-// vmesto typeof id === 'string'
-function logId_2(id: string | number) {
-  if (isString(id)) {
-    console.log(id); // string
-  } else {
-    console.log(id); // number
-  }
-}
-
-// bolee slozhny variant ispolzovanya type guard
-
-// sperva functiya type guard
 function isAdmin(user: User | Admin): user is Admin {
   return 'role' in user;
 }
@@ -51,9 +19,4 @@ function setRoleZero(user: User | Admin) {
   } else {
     throw new Error('пользователь не Админ');
   }
-}
-
-// vtoroi variant napisaniya isAdmin
-function isAdminAlternative(user: User | Admin): user is Admin {
-  return (user as Admin).role !== undefined; // cherez prinuditelnoe castovanie
 }
