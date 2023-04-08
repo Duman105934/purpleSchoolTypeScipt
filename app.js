5a751cdb3c15d44d657d915ad1332e1813dae943
Duman Makulbekov <makulbekov.d@gmail.com>
2023-04-08 13:33:37 +0600

    036 Конструктор

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 "use strict";
 class User {
-    constructor(nameConstuctor) {
-        this.userName = nameConstuctor;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === 'string') {
+            this.userName = ageOrName;
+        }
+        else if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
-const user = new User('Vasya');
-console.log(user);
-user.userName = 'Petya';
-console.log(user);
+const user_1 = new User('Vasya');
+const user_2 = new User();
+const user_3 = new User(33);
+const user_4 = new User('Vasya', 33);
--- app.ts
+++ app.ts
@@ -1,12 +1,24 @@
 class User {
   userName: string;
+  age: number;
 
-  constructor(nameConstuctor: string) {
-    this.userName = nameConstuctor;
+  constructor();
+  constructor(name: string);
+  constructor(age: number);
+  constructor(name: string, age: number);
+  constructor(ageOrName?: string | number, age?: number) {
+    if (typeof ageOrName === 'string') {
+      this.userName = ageOrName;
+    } else if (typeof ageOrName === 'number') {
+      this.age = ageOrName;
+    }
+    if (typeof age === 'number') {
+      this.age = age;
+    }
   }
 }
 
-const user = new User('Vasya');
-console.log(user);
-user.userName = 'Petya';
-console.log(user);
+const user_1 = new User('Vasya');
+const user_2 = new User();
+const user_3 = new User(33);
+const user_4 = new User('Vasya', 33);
